f291d1735585710221a573b90e63198fb9898d96
Kasemaa <92432832+Kasemaa@users.noreply.github.com>
2021-12-08 11:34:25 +0200

    added getBook functional by UI and LS

--- classes/app.js
+++ classes/app.js
@@ -6,6 +6,18 @@ const ls = new LS()
 const form = document.querySelector('#book-form')
 form.addEventListener('submit', addBook)
 
+// page reload
+document.addEventListener('DOMContentLoaded', getBooks)
+
+function getBooks(){
+    // get data from LS
+    const books = ls.getData('books')
+    // for each book in books
+    books.forEach(function (booksFromLS){
+        ui.addBook(booksFromLS)
+    })
+}
+
 // addBook function
 function addBook(event){
     // get form data from form input
